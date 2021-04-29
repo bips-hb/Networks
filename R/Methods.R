fab8483c6d76c7c6cbfc092b16c12c9939c3afa7
Arne Godt <arne23@hotmail.de>
2021-04-29 09:27:09 +0200

    Neu

--- R/Methods.R
+++ R/Methods.R
@@ -1,2 +1,23 @@
+#JGL tests
+
+#http://www.benoslab.pitt.edu/manatakisECCB2018.html
+#http://www.benoslab.pitt.edu/Software/pimgm/  
+#for another paper, java script
+
+library(JGL)
+library(igraph)
+data(example.data)
+example1 <- example.data[[1]]
+example2 <- example.data[[2]]
+
+example1 <- example1[1:10,1:20]
+example2 <- example2[1:10,1:20]
+all <- list(example1,example2)
+
+jpltest <- JGL(all, lambda1 = 0.1, lambda2 = 0.1)
+plot(jpltest,layout = layout_on_grid())
+plot.jgl()
+
 library(huge)
-asdas
+library(fastclime)
+library(flare)
